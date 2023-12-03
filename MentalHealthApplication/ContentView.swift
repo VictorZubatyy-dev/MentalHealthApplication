4a4139c52b84017d1c09561aed364e64acad3581
Victor Zubatyy <D21125389@mytudublin.ie>
2023-12-03 13:58:29 +0000

    Initial Commit

---
+++ MentalHealthApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MentalHealthApplication
+//
+//  Created by Victor on 03/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MentalHealthApplication/MentalHealthApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalHealthApplicationApp.swift
+//  MentalHealthApplication
+//
+//  Created by Victor on 03/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MentalHealthApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
