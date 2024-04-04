d434afb18a886766de7568d9be499d66006144b3
Victor Zubatyy <D21125389@mytudublin.ie>
2024-04-04 16:25:57 +0100

    HealthView, LogView,  Journal View, ML design/functionality improvements  and Journaling Suggestions API and Local Notifications integration - HealthView now has confetti upon completion of either the calories/exericise goal for the day     - Sleep score is now active and fully functional
    - LogView logs entry background colour based on mood has been removed due to glitches and has been replaced with a border
    
    - JournalViews edit entry view now has a redesign UI that becomes a scroll view when selecting alcohol/caffeine for more space
        - JournalingSuggestion button added
        - Removed photo picker as Journaling Suggestions API has access to photos
        - Added a another textfield for the suggestion title
        - Default log textfield has text that offers the user the option to reflect on their feelings/experience whilst listening to music
    
    - ML is now fully functional
        - ML sleep score prediction now uses all the users actual onboarding and journal values e.g sleep schedule, total caffeine/alcohol for the day
    
    - Journaling Suggestions API framework integrated
        - Journaling Suggestions (an advanced ML recommendations algorithm) provides the user an in depth retrospective prompt:
            - The journaling suggestion button provides this recommendation for the user if they are not sure what to write about
            - For the moment the recommendation works for recent, recommended music listened to on Apple music with a max of 2 songs that the user may choose
            - Other recommendations chosen only provide the title for now

---
+++ MentalHealthApplication/Views/Songs.swift
@@ -0,0 +1,8 @@
+//
+//  Songs.swift
+//  MentalHealthApplication
+//
+//  Created by Victor on 31/03/2024.
+//
+
+import Foundation
---
+++ MentalHealthApplication/Views/SuggestionView.swift
@@ -0,0 +1,18 @@
+//
+//  SuggestionView.swift
+//  MentalHealthApplication
+//
+//  Created by Victor on 29/03/2024.
+//
+
+import SwiftUI
+
+struct SuggestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuggestionView()
+}
